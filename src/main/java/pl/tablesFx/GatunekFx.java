475e8ea265171c75ed2e93aa908e3adb43fab47e
wybryk <3odaczek@gmail.com>
2017-04-22 18:05:21 +0200

    Add files via upload

---
+++ src/main/java/pl/tablesFx/GatunekFx.java
@@ -0,0 +1,43 @@
+package pl.tablesFx;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Created by Mateusz on 2017-04-22.
+ */
+public class GatunekFx {
+    private IntegerProperty id = new SimpleIntegerProperty();
+    private StringProperty nazwa = new SimpleStringProperty();
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getNazwa() {
+        return nazwa.get();
+    }
+
+    public StringProperty nazwaProperty() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa.set(nazwa);
+    }
+
+    @Override
+    public String toString() {
+        return  nazwa.getValue();
+    }
+}
